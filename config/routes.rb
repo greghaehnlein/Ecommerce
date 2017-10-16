0c752db476084e0c5addbddc6eb7a8ac7c3728b0
Greg Haehnlein <greghaehnlein@gmail.com>
2017-10-16 17:31:56 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'storefront#all_items'
 
-  get  'categorial' => 'storefront#items_by_category'
+  get  'categorical' => 'storefront#items_by_category'
 
   get 'branding' => 'storefront#items_by_brand'
 
